6c61732c0ceb3cde32422c77bf202f557569f8a0
Utkarsh Verma <utkarshverma759@gmail.com>
2024-09-04 23:33:28 +0530

    Backend started  working

--- src/components/AI virtuall world/AIvirtuallworld.jsx
+++ src/components/AI virtuall world/AIvirtuallworld.jsx
@@ -33,48 +33,48 @@ const AIvirtuallworld = () => {
   };
 
   return (
-    <div className='aibox'>
-    <div className='mains'>
-      <div className='aivid'>
-        <video src={Vid} controls loop autoPlay muted></video>
-      </div>
-      <div className='recomend'>
-        <input
-        id='fileInput'
-          className='upload'
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-        <h3> Clich herer to Upload Dress Pic</h3>
-        <label htmlFor="fileInput" className='btnUpload'>
-          Choose Image 
-        </label>
-        <br />
-        <button className='btn btnRec' onClick={handleUpload}>
-          Upload and Recommend
-        </button>
+    <div className="aibox">
+      <div className="mains">
+        <div className="aivid">
+          <video src={Vid} controls loop autoPlay muted></video>
+        </div>
+        <div className="recomend">
+          <input
+            id="fileInput"
+            className="upload"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+          <h3> Clich herer to Upload Dress Pic</h3>
+          <label htmlFor="fileInput" className="btnUpload">
+            Choose Image
+          </label>
+          <br />
+          <button className="btn btnRec" onClick={handleUpload}>
+            Upload and Recommend
+          </button>
 
-        <div>
-          <h2>Recommendations:</h2>
-          {recommendations.length > 0 ? (
-            <ul>
-              {recommendations.map((recommendation, index) => (
-                <li key={index}>
-                  <img
-                    src={`/${recommendation}`}
-                    alt={`Recommendation ${index}`}
-                    style={{ maxWidth: '100px', maxHeight: '100px' }}
-                  />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No recommendations available.</p>
-          )}
+          <div>
+            <h2>Recommendations:</h2>
+            {recommendations.length > 0 ? (
+              <ul>
+                {recommendations.map((recommendation, index) => (
+                  <li key={index}>
+                    <img
+                      src={`http://127.0.0.1:5000${recommendation}`}
+                      alt={`Recommendation ${index}`}
+                      style={{ maxWidth: "100px", maxHeight: "100px" }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No recommendations available.</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
